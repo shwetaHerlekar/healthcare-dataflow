fbaaa4a2ba6dd60e68d69f6965e62ec3124acdff
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-04-04 18:58:08 +0530

    bigtable add

--- src/main/java/com/example/Synpuf.java
+++ src/main/java/com/example/Synpuf.java
@@ -33,6 +33,7 @@ import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.client.Mutation
 
 public class Synpuf
 {
