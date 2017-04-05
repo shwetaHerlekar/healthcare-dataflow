b0fc68613f3be2c2852c6a3effa197618850e2a1
Shweta Herlekar <shweta_herlekar@persistent.co.in>
2017-04-05 14:07:24 +0530

    bigtable add

--- src/main/java/com/example/Synpuf.java
+++ src/main/java/com/example/Synpuf.java
@@ -25,6 +25,7 @@ import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.AbstractBigtableConnection;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.Put;
